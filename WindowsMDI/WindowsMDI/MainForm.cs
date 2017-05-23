57f439a8f1b9ae55094444c8da3ecc2b1788671e
KuboTo <wangdengcheng1993@163.com>
2017-05-23 14:13:30 +0800

    MDI多窗体
    MDI多窗体

---
+++ WindowsMDI/WindowsMDI/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsMDI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void 新建ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm myChildForm = new ChildForm();
+            myChildForm.MdiParent = this;
+            myChildForm.Show();
+        }
+    }
+}
